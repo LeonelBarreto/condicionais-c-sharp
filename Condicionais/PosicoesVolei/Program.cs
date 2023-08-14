ac8842851afc11b1cf899b7b1d8126c6a78b2e1b
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 14:03:06 -0300

    resolvido: posição vôlei

---
+++ Condicionais/PosicoesVolei/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PosicoesVolei;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int alturaCandidatoEmCm = int.Parse(Console.ReadLine());
+
+        if (alturaCandidatoEmCm < 180)
+        {
+            Console.WriteLine("Reprovado!");
+        }
+        else if(alturaCandidatoEmCm <= 185)
+        {
+            Console.WriteLine("Libero");
+        }
+        else if (alturaCandidatoEmCm <= 195)
+        {
+            Console.WriteLine("Ponteiro");
+        }
+        else if (alturaCandidatoEmCm <= 205)
+        {
+            Console.WriteLine("Oposto");
+        }
+        else
+        {
+            Console.WriteLine("Central");
+        }
+    }
+}
+
