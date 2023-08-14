5bcd2e60009419311213aca31b00c0ea6e59dd38
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 15:22:56 -0300

    resolvido: nota em conceito

---
+++ Condicionais/NotaEmConceito/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NotaEmConceito;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira sua nota:");
+        float nota = float.Parse(Console.ReadLine());
+
+        if (nota < 4)
+        {
+            Console.WriteLine("O estudante obteve o conteito E.");
+        }
+        else if (nota < 6 )
+        {
+            Console.WriteLine("O estudante obteve o conteito D.");
+        }
+        else if (nota < 8)
+        {
+            Console.WriteLine("O estudante obteve o conteito C.");
+        }
+        else if (nota < 9)
+        {
+            Console.WriteLine("O estudante obteve o conteito B.");
+        }
+        else if (nota < 10)
+        {
+            Console.WriteLine("O estudante obteve o conteito A.");
+        }
+        else
+            Console.WriteLine("Por favor inserir um número entre 0 e 10.");
+    }
+}
+
