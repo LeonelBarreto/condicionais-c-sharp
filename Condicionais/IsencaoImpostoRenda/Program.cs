b47521a55d97d707d30d69d863e961845964e1dd
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 14:54:15 -0300

    resolvido: isenção imposto de renda

---
+++ Condicionais/IsencaoImpostoRenda/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IsencaoImpostoRenda;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool aposentado = bool.Parse(Console.ReadLine());
+        bool portadorDeDoenca = bool.Parse(Console.ReadLine());
+        float rendimentoAnual = float.Parse(Console.ReadLine());
+
+        if (aposentado == true || portadorDeDoenca == true)
+        {
+            Console.WriteLine("Isento/a!");
+        }
+        else if (rendimentoAnual < 28559.70)
+        {
+            Console.WriteLine("Vaza, Leão! Já está difícil sem você!");
+        }
+        else
+        {
+            Console.Write("Pega, Leão!");
+        }
+    }
+}
+
