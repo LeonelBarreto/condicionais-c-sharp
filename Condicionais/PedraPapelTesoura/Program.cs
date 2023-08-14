48f2d58abacbbcae6bdb46328711b1c5e4b46bb0
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 11:54:20 -0300

    resolvido: pedra, papel ou tesoura

--- Condicionais/PedraPapelTesoura/Program.cs
+++ Condicionais/PedraPapelTesoura/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string jogador1 = Console.ReadLine();
+        string jogador2 = Console.ReadLine();
+
+        if (jogador1 == jogador2)
+        {
+            Console.WriteLine("EMPATE");
+        }
+        else if (jogador1 == "Papel" && jogador2 == "Pedra")
+        {
+            Console.WriteLine("Jogador 1 venceu");
+        }
+        else if (jogador1 == "Pedra" && jogador2 == "Tesoura")
+        {
+            Console.WriteLine("Jogador 1 venceu");
+        }
+        else if (jogador1 == "Tesoura" && jogador2 == "Papel")
+        {
+            Console.WriteLine("Jogador 1 venceu");
+        }
+        else if (jogador1 == "Pedra" && jogador2 == "Papel")
+        {
+            Console.WriteLine("Jogador 2 venceu");
+        }
+        else if (jogador1 == "Tesoura" && jogador2 == "Pedra")
+        {
+            Console.WriteLine("Jogador 2 venceu");
+        }
+        else if (jogador1 == "Papel" && jogador2 == "Tesoura")
+        {
+            Console.WriteLine("Jogador 2 venceu");
+        }
+        else
+        {
+            Console.WriteLine("Por favor, inserir Pedra, Papel ou Tesoura.");
+        }
     }
 }
 
