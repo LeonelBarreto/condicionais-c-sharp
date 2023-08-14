0ab7e0713ee6449bbbcc9af04b1c04427bef3b99
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 12:03:54 -0300

    resolvido: par ou impar

---
+++ Condicionais/ParOuImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ParOuImpar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int jogador1 = int.Parse(Console.ReadLine());
+        int jogador2 = int.Parse(Console.ReadLine());
+
+        int soma = jogador1 + jogador2;
+        int resto = soma % 2;
+
+        if(resto == 0)
+        {
+            Console.WriteLine("PAR!");
+        }
+        else
+        {
+            Console.WriteLine("ÍMPAR!");
+        }
+    }
+}
+
