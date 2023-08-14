610503cb897f45a5949f22e44f406364c246615d
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 12:09:43 -0300

    resolvido: dominó

---
+++ Condicionais/Domino/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Domino;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int ladoA = int.Parse(Console.ReadLine());
+        int ladoB = int.Parse(Console.ReadLine());
+
+        if (ladoA == ladoB)
+        {
+            Console.WriteLine("Sim!");
+        }
+        else
+        {
+            Console.WriteLine("Não!");
+        }
+    }
+}
+
