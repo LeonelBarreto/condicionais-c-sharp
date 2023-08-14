3174c6716c59a57502d352dd885b324d8855c04f
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 15:12:16 -0300

    resolvido: montanha russa

---
+++ Condicionais/MontanhaRussa/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MontanhaRussa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira sua idade (apenas números):");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Alguma patologia? Insira apenas true ou false:");
+        bool patologia = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira sua altura (apenas números inteiros) em centimetros:");
+        int alturaMinimaEmCm = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Você é estudante? Insira apenas true ou false:");
+        bool estudante = bool.Parse(Console.ReadLine());
+
+        if (idade < 12 || idade >=65 || patologia == true || alturaMinimaEmCm < 150)
+        {
+            Console.WriteLine("Acesso Negado!");
+        }
+        else if (idade < 18 || estudante == true)
+        {
+            Console.WriteLine("10 Reais!");
+        }
+        else
+        {
+            Console.WriteLine("20 Reais");
+        }
+    }
+}
+
