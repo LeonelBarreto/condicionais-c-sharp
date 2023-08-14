417749cdee9f6b1c2a2512c3c9fa0f34f5a69432
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-14 13:53:03 -0300

    resolvido: peneira volei

---
+++ Condicionais/PeneiraDeVolei/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace PeneiraDeVolei;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int alturaCandidatoEmCm = int.Parse(Console.ReadLine());
+     
+        if (alturaCandidatoEmCm >= 180)
+        {
+            Console.WriteLine("Aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado!");
+        }
+    }
+}
+
