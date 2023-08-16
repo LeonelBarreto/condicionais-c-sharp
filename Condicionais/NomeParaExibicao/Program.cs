214201516ae08248ff64703ab3ed56d5a652322c
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-16 10:27:41 -0300

    resolvido: rematrícula escolar

---
+++ Condicionais/NomeParaExibicao/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NomeParaExibicao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insira o seu primeiro nome (ou não):");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Insira o seu sobrenome (ou não):");
+        string sobrenome = Console.ReadLine();
+
+        Console.WriteLine("Insira o seu apelido (ou não):");
+        string apelido = Console.ReadLine();
+
+        if (nome)
+        {
+            Console.WriteLine($"{nome}");
+        }
+        else if (nome && sobrenome)
+        {
+            Console.WriteLine($"{nome} {sobrenome}");
+        }
+        else if (apelido)
+        {
+            Console.WriteLine($"{apelido}");
+        }
+        else
+        {
+            Console.WriteLine("Você deve inserir pelo menos nome, sobrenome ou apelido.");
+        }
+    }
+}
+
---
+++ Condicionais/RematriculaEscolar/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace RematriculaEscolar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a idade do aluno:");
+        int idadeAluno = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ele está acompanhado de um responsável? Insira 'true'para verdadeiro e 'false' para falso.");
+        bool acompanhadoResponsavel = bool.Parse(Console.ReadLine());
+
+        if(idadeAluno >= 18)
+        {
+            Console.WriteLine("Rematrícula realizada com sucesso.");
+        }
+        else if(idadeAluno < 18 && acompanhadoResponsavel == true)
+        {
+            Console.WriteLine("Rematrícula realizada com sucesso.");
+        }
+        else if (idadeAluno < 18 && acompanhadoResponsavel == false)
+        {
+            Console.WriteLine("Não é possível refazer a matrícula.");
+        }
+        else
+        {
+            Console.WriteLine("Por favor, inserir uma idade válida e informar se está acompanhado de um responsável usando apenas 'true' ou 'false'.");
+        }
+    }
+}
+
